e16bac1f637e9c5ef381e9a6ad1a73686528ec83
McDoyen <mrpoloh@yahoo.co.uk>
2017-03-27 11:55:53 +0300

    Update unit tests

--- src/components/__tests__/Badge.spec.ts
+++ src/components/__tests__/Badge.spec.ts
@@ -7,31 +7,76 @@ import { Alert } from "../Alert";
 describe("Badge", () => {
     const createBadge = (props: BadgeProps) => shallow(createElement(Badge, props));
 
-    describe("should render the structure", () => {
-        it("for badge", () => {
-            const badgeProps: BadgeProps = { badgeValue: "0", label: "default" };
-            const badgeComponent = createBadge(badgeProps);
+    it("should render the structure", () => {
+        const badgeProps: BadgeProps = {
+            label: "Custom Label",
+            onClickAction: jasmine.createSpy("onClick"),
+            value: "0"
+        };
+        const badge = createBadge(badgeProps);
 
-            expect(badgeComponent).toBeElement(
-                DOM.div(
-                    {
-                        className: "widget-badge-display"
-                    },
-                    DOM.span({ className: "widget-badge-text" }, badgeProps.label),
-                    DOM.span({ className: "widget-badge badge label-default" }, badgeProps.badgeValue),
-                    createElement(Alert)
-                )
-            );
-        });
+        expect(badge).toBeElement(
+            DOM.div(
+                {
+                    className: "widget-badge-display",
+                    onClick: jasmine.any(Function) as any
+                },
+                DOM.span({ className: "widget-badge-text" }, badgeProps.label),
+                DOM.span({ className: "widget-badge badge label-default" }, badgeProps.value),
+                createElement(Alert)
+            )
+        );
     });
 
-    it("should render with style 'success'", () => {
-        const badgeProps: BadgeProps = {
-            style: "success"
-        };
+    it("with click function should respond to click event", () => {
+        const badgeProps: BadgeProps = { onClickAction: jasmine.createSpy("onClick") };
+        const onClick = badgeProps.onClickAction = jasmine.createSpy("onClick");
+        const badge = createBadge(badgeProps);
 
-        const badgeComponent = createBadge(badgeProps);
+        badge.simulate("click");
 
-        expect(badgeComponent.childAt(1).hasClass("widget-badge badge label-success")).toBe(true);
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+
+    it("with style default should render with class btn-default", () => {
+        const badgeProps: BadgeProps = { style: "default" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-default")).toBe(true);
+    });
+
+    it("with style default should render with class btn-primary", () => {
+        const badgeProps: BadgeProps = { style: "primary" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-primary")).toBe(true);
+    });
+
+    it("with style default should render with class btn-success", () => {
+        const badgeProps: BadgeProps = { style: "success" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-success")).toBe(true);
+    });
+
+    it("with style default should render with class btn-sucinfocess", () => {
+        const badgeProps: BadgeProps = { style: "info" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-info")).toBe(true);
+    });
+
+    it("with style default should render with class btn-warning", () => {
+        const badgeProps: BadgeProps = { style: "warning" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-warning")).toBe(true);
+    });
+
+    it("with style default should render with class btn-danger", () => {
+        const badgeProps: BadgeProps = { style: "danger" };
+        const badge = createBadge(badgeProps);
+
+        expect(badge.childAt(1).hasClass("label-danger")).toBe(true);
     });
 });
