bec26d4a4a70a0e0f0f77c5a1334574ed0634bfe
McDoyen <mrpoloh@yahoo.co.uk>
2017-04-18 13:54:18 +0300

    Fix code style

--- src/components/Badge.ts
+++ src/components/Badge.ts
@@ -1,28 +1,23 @@
 import { DOM, createElement } from "react";
-
 import * as classNames from "classnames";
-import { Alert } from "./Alert";
 
 import "../ui/Badge.css";
 
 export interface BadgeProps {
-	alertMessage?: string;
-	label?: string;
-	value?: string;
-	style?: string;
-	clickable?: boolean;
-	notClickable?: boolean;
-	onClickAction?: () => void;
+    label?: string;
+    value?: string;
+    style?: string;
+    clickable?: boolean;
+    onClickAction?: () => void;
 }
 
 export const Badge = (props: BadgeProps) =>
-	createElement("div", {
-			className: classNames("widget-badge", { "widget-badge-link": props.clickable }),
-			onClick: props.onClickAction
-		},
-		DOM.span({ className: "widget-badge-text" }, props.label),
-		DOM.span({
-			className: classNames("widget-badge", "badge", { [`label-${props.style}`]: !!props.style })
-		}, props.value),
-		createElement(Alert, { message: props.alertMessage })
-	);
+    createElement("div", {
+        className: classNames("widget-badge", { "widget-badge-link": props.clickable }),
+        onClick: props.onClickAction
+    },
+        DOM.span({ className: "widget-badge-text" }, props.label),
+        DOM.span({
+            className: classNames("widget-badge", "badge", { [`label-${props.style}`]: !!props.style })
+        }, props.value)
+    );
--- src/components/__tests__/Alert.spec.ts
+++ src/components/__tests__/Alert.spec.ts
@@ -4,18 +4,18 @@ import { DOM, createElement } from "react";
 import { Alert } from "../Alert";
 
 describe("Alert", () => {
-	it("renders the structure when an alert message is specified", () => {
-		const message = "This is an error";
-		const alert = shallow(createElement(Alert, { message }));
+    it("renders the structure when an alert message is specified", () => {
+        const message = "This is an error";
+        const alert = shallow(createElement(Alert, { message }));
 
-		expect(alert).toBeElement(
-			DOM.div({ className: "alert alert-danger widget-badge-alert" }, message)
-		);
-	});
+        expect(alert).toBeElement(
+            DOM.div({ className: "alert alert-danger widget-badge-alert" }, message)
+        );
+    });
 
-	it("renders no structure when the alert message is not specified", () => {
-		const alert = shallow(createElement(Alert));
+    it("renders no structure when the alert message is not specified", () => {
+        const alert = shallow(createElement(Alert));
 
-		expect(alert).toBeElement(null);
-	});
+        expect(alert).toBeElement(null);
+    });
 });
